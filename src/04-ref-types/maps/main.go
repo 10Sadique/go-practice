84d1256fbd66066bfabf55e2aaf4c431e22f02e8
10Sadique <jafarsjahan@gmail.com>
2023-07-25 10:55:17 +0600

    map parctice

--- src/04-ref-types/maps/main.go
+++ src/04-ref-types/maps/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var map1 map[int]int
+	if map1 == nil {
+		fmt.Println("True")
+	} else {
+		fmt.Println("False")
+	}
+
+	map2 := map[int]string{
+		90: "Dog",
+		91: "Cat",
+		92: "Cow",
+		93: "Bird",
+		94: "Rabbit",
+	}
+
+	fmt.Println("Map-2: ", map2)
+	fmt.Println(map2[90])
+	fmt.Println(len(map2))
+
+	delete(map2, 91)
+	fmt.Println("Map-2: ", map2)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("Map", n)
 }
